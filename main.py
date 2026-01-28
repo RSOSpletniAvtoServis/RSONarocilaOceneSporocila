94cd73dd1d30734b55f972a243b78381712f5568
mangart <zan.gostic1@gmail.com>
2026-01-28 01:00:53 +0100

    fix

--- main.py
+++ main.py
@@ -178,9 +178,9 @@ def get_narocila(nar: Narocilo1):
                             "IDZnamka": row[10],
                             "IDPonudba": row[11],
                             "NazivZnamke": vozila.get(row[8], {}).get("NazivZnamke", row[8]) or row[10],
-                            "NazivModel": vozila.get(row[8], {}).get("NazivModel", row[8]) or row[9],
-                            "NazivPoslovalnice": poslovalnice.get(row[5], {}).get("NazivPoslovalnice", row[5]) or row[5],
-                            "NazivStoritve": storitve.get(row[6], {}) or row[6]
+                            "NazivModel": vozila.get(str(row[8]), {}).get("NazivModel", str(row[8])) or row[9],
+                            "NazivPoslovalnice": poslovalnice.get(str(row[5]), {}).get("NazivPoslovalnice", str(row[5])) or row[5],
+                            "NazivStoritve": storitve.get(str(row[6]), {}) or row[6]
                         } 
                             for row in rows ]
 
