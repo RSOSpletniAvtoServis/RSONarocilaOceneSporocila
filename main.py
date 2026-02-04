ba94999dde841df579b33fb4add661d9d0a5f3bb
mangart <zan.gostic1@gmail.com>
2026-02-04 13:34:10 +0100

    changed host

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ try:
     pool = mysql.connector.pooling.MySQLConnectionPool(
         pool_name="mypool",
         pool_size=5,
-        host="34.44.150.229",
+        host="127.0.0.1",                   #"34.44.150.229",
         user="zan",
         password=">tnitm&+NqgoA=q6",
         database="RSOPoslovalnicaZaposleni",
